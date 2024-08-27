5d4dd399e43d997be8394629b71e0b023061dcf6
Lucas <80072037+lucashsousa@users.noreply.github.com>
2024-08-27 00:36:04 +0000

    Codigos aula 26/08/2024

---
+++ aula05/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program{
+    public static void Main(String[] args){
+        //Conversão implícita
+        int numInt = 45;
+        double numDouble = numInt;
+        Console.WriteLine("Conversão implícita de int para double: ");
+        Console.WriteLine($"Int: {numInt}, Double: {numDouble}");
+
+        //Conversão usando o Convert
+        double valorDouble = 7.45;
+        int valorConvertidoInt = Convert.ToInt32(valorDouble);
+        string textoValor = "123";
+        int textoConvertidoInt = Convert.ToInt32(textoValor);
+        Console.WriteLine($"Double: {valorDouble}, Inteiro: {valorConvertidoInt}, String: {textoValor}, Inteiro: {textoConvertidoInt}");
+
+    }
+}
